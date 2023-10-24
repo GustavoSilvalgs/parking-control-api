9156aa551c9b39102db51a05f109110328e38674
GustavoSilvalgs <luisgust06@gmail.com>
2023-10-23 22:19:58 -0300

    feat(api): Add POST method for creating parking spot
    Added a POST method to create a parking spot in the API. Includes validations to prevent duplicates.

--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,13 +1,33 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ParkingSpotService {
 
-    final ParkingSpotService service;
+    final ParkingSpotRepository repository;
 
-    public ParkingSpotService(ParkingSpotService service) {
-        this.service = service;
+    public ParkingSpotService(ParkingSpotRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return repository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return repository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return repository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return repository.existsByApartmentAndBlock(apartment, block);
     }
 }
