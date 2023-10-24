51675cf28970f0fdd73fb93ea441f476ec72f18f
GustavoSilvalgs <luisgust06@gmail.com>
2023-10-24 12:26:13 -0300

    feat(security): Add initial security configuration
    Added initial security configuration, disabling CSRF and configuring session management to STATELESS.

---
+++ parking-control/src/main/java/com/api/parkingcontrol/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.api.parkingcontrol.repositories;
+
+import com.api.parkingcontrol.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<UserModel, String> {
+
+    UserDetails findByLogin(String login);
+}
